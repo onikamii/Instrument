1c3682caca3a4d9a59f41c8bb59e09c4db24288e
User <User@DESKTOP-SNB119S>
2023-02-06 23:00:37 +0300

    Добавьте файлы проекта.

---
+++ Инструмент/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Инструмент
+{
+    interface Instrument
+    {
+        void play();
+    }
+    class Guitar: Instrument
+    {
+        int colvostrings;
+        public void play()
+        {
+
+        }
+    }
+    class Drum: Instrument
+    {
+        int size;
+        public void play()
+        {
+
+        }
+    }
+    class Trumpet: Instrument
+    {
+        int diameter;
+        public void play()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guitar guitar = new Guitar();
+            Drum drum = new Drum();
+            Trumpet trumpet = new Trumpet();
+            string KEY = "До мажор";
+            Instrument[] massiv = { };
+            foreach (Instrument i in massiv)
+            {
+                guitar.play();
+                drum.play();
+                trumpet.play();
+            }
+        }
+    }
+}
